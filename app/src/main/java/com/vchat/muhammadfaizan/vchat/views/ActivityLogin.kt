e7c3fa397e0cc0753c35d3df329125d7212e749e
Faizan <mohammadfaizan980@gmail.com>
2018-10-28 12:00:06 +0500

    Added facebook login

--- app/src/main/java/com/vchat/muhammadfaizan/vchat/views/ActivityLogin.kt
+++ app/src/main/java/com/vchat/muhammadfaizan/vchat/views/ActivityLogin.kt
@@ -5,10 +5,17 @@ import android.os.Bundle
 import android.support.design.widget.TextInputEditText
 import android.support.design.widget.TextInputLayout
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.Toast
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -17,11 +24,9 @@ import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.database.ChildEventListener
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.vchat.muhammadfaizan.vchat.R
 
@@ -34,7 +39,11 @@ class ActivityLogin : AppCompatActivity() {
     lateinit var btnRegister: Button
     lateinit var progressBar: ProgressBar
     lateinit var firebaseAuth: FirebaseAuth
-    lateinit var btnSignIn: SignInButton
+    lateinit var btnSignInGoogle: SignInButton
+    lateinit var btnSignInFacebook: LoginButton
+    lateinit var callbackManager: CallbackManager
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -51,7 +60,8 @@ class ActivityLogin : AppCompatActivity() {
         btnRegister = findViewById(R.id.btnRegister)
         progressBar = findViewById(R.id.pBarLogin)
         firebaseAuth = FirebaseAuth.getInstance()
-        btnSignIn = findViewById(R.id.btnGoogle)
+        btnSignInGoogle = findViewById(R.id.btnGoogle)
+        btnSignInFacebook = findViewById(R.id.btnFB)
     }
 
     override fun onStart() {
@@ -63,8 +73,32 @@ class ActivityLogin : AppCompatActivity() {
     }
 
     fun loginUser() {
+        //Facebook Login
+        callbackManager = CallbackManager.Factory.create()
 
-        btnSignIn.setOnClickListener(object : View.OnClickListener {
+        btnSignInFacebook.setReadPermissions("email", "public_profile")
+        btnSignInFacebook.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                Log.d("tg", "facebook:onSuccess:$loginResult")
+                handleFacebookAccessToken(loginResult.accessToken)
+                progressBar.visibility = View.VISIBLE
+            }
+
+            override fun onCancel() {
+                Log.d("tg", "facebook:onCancel")
+                progressBar.visibility = View.INVISIBLE
+                // ...
+            }
+
+            override fun onError(error: FacebookException) {
+                progressBar.visibility = View.INVISIBLE
+                Log.d("tg", "facebook:onError", error)
+                // ...
+            }
+        })
+
+        //Google Login
+        btnSignInGoogle.setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
                 progressBar.visibility = View.VISIBLE
                 googleSignIn()
@@ -135,6 +169,7 @@ class ActivityLogin : AppCompatActivity() {
     public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         progressBar.visibility = View.INVISIBLE
+        callbackManager.onActivityResult(requestCode, resultCode, data)
 
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == 2) {
@@ -176,5 +211,36 @@ class ActivityLogin : AppCompatActivity() {
                 }
             }
         })
+    }
+
+    private fun handleFacebookAccessToken(token: AccessToken) {
+        Log.d("tg", "handleFacebookAccessToken:$token")
+
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        FirebaseDatabase.getInstance().getReference("Users").child(FirebaseAuth.getInstance().uid!!).child("id").setValue(FirebaseAuth.getInstance().uid).addOnCompleteListener(object : OnCompleteListener<Void> {
+                            override fun onComplete(p0: Task<Void>) {
+                                if (p0.isSuccessful) {
+                                    startActivity(Intent(applicationContext, MainActivity::class.java))
+                                    finish()
+                                } else {
+                                    Toast.makeText(applicationContext, p0.exception?.message.toString(), Toast.LENGTH_LONG).show()
+                                    progressBar.visibility = View.INVISIBLE
+                                }
+                            }
+                        })
+
+                    } else {
+                        progressBar.visibility = View.INVISIBLE
+                        // If sign in fails, display a message to the user.
+                        Log.w("tg", "signInWithCredential:failure", task.exception)
+                        Toast.makeText(baseContext, task.exception?.message.toString(),
+                                Toast.LENGTH_SHORT).show()
+                    }
+
+                }
     }
 }
