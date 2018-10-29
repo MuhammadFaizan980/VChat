d8aba8b41d8cce77e60abed36dff48de8524f698
Faizan <mohammadfaizan980@gmail.com>
2018-10-29 21:36:04 +0500

    Fixed auth bug

--- app/src/main/java/com/vchat/muhammadfaizan/vchat/views/MainActivity.kt
+++ app/src/main/java/com/vchat/muhammadfaizan/vchat/views/MainActivity.kt
@@ -1,17 +1,12 @@
 package com.vchat.muhammadfaizan.vchat.views
 
-import android.content.DialogInterface
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentTransaction
-import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
-import android.view.View
 import android.widget.ProgressBar
-import android.widget.Toast
-import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.vchat.muhammadfaizan.vchat.R
 
@@ -29,7 +24,6 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         initViews()
         setFragment(HomeFragment())
         setListener()
-        checkEmail()
     }
 
     private fun initViews() {
@@ -69,35 +63,5 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         var setFragObj: FragmentTransaction = supportFragmentManager.beginTransaction()
         setFragObj.replace(R.id.mFrame, fragment)
         setFragObj.commit()
-    }
-
-    private fun checkEmail() {
-
-        if (intent.extras.get("provider").equals("email")) {
-            if (!firebaseAuth.currentUser!!.isEmailVerified) {
-                var dialog: AlertDialog.Builder = AlertDialog.Builder(this)
-                dialog.setCancelable(false)
-                dialog.setTitle("Warning!")
-                dialog.setMessage("Your email is not verified\nPlease verify your email by clicking Verify button")
-                dialog.setPositiveButton("Verify", DialogInterface.OnClickListener { dialogInterface, i ->
-                    progressbar.visibility = View.VISIBLE
-                    firebaseAuth.currentUser!!.sendEmailVerification().addOnCompleteListener { task: Task<Void> ->
-                        if (task.isSuccessful) {
-                            progressbar.visibility = View.INVISIBLE
-                            Toast.makeText(applicationContext, "Verification link sent to your email\nPlease verify as soon as possible", Toast.LENGTH_LONG).show()
-                            dialogInterface.cancel()
-                        } else {
-                            progressbar.visibility = View.INVISIBLE
-                            Toast.makeText(applicationContext, task.exception?.message.toString(), Toast.LENGTH_LONG).show()
-                            dialogInterface.cancel()
-                        }
-                    }
-                }).setNegativeButton("Cancel", DialogInterface.OnClickListener { dialogInterface, i ->
-                    dialogInterface.cancel()
-                })
-
-                dialog.show()
-            }
-        }
     }
 }
