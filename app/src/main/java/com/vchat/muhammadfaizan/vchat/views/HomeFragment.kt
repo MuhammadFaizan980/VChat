811a1cc2c0f1f791088f4c96f3356df27e4a63e7
Faizan <mohammadfaizan980@gmail.com>
2018-11-13 16:36:57 +0500

    Added message screen UI but not backend

--- app/src/main/java/com/vchat/muhammadfaizan/vchat/views/HomeFragment.kt
+++ app/src/main/java/com/vchat/muhammadfaizan/vchat/views/HomeFragment.kt
@@ -9,22 +9,11 @@ import android.view.ViewGroup
 
 import com.vchat.muhammadfaizan.vchat.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        var view : View = inflater.inflate(R.layout.fragment_home, container, false)
+        return view
     }
-
-
 }
